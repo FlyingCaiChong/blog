22e62a3901a90acbc8359f2edfb609475b857a6c
方恒 <fangheng@shwpg.com>
2023-01-17 11:30:15 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Blog", // 网站的标题
+  description: "Just a Blog", // 网站的描述
+  base: "/", // 部署站点的基础路径
+  locales: {
+    // 提供多语言支持的语言配置
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  theme: "vdoing",
+};
