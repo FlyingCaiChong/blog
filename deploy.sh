bf7b2b02674e5b006466fac56b1b75ae4c7f613d
方恒 <951412526@qq.com>
2023-07-01 15:59:42 +0800

    ci: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,16 +3,25 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+echo "开始打包"
+
 # 生存静态文件
 yarn docs:build
 
-# 进入生成的文件夹
+echo "打包结束"
+
+# # 进入生成的文件夹
 cd ./docs/.vuepress/dist
+
+echo "进入生成的文件夹"
 
 git init
 git branch -M master
-git remote add origin https://github.com/FlyingCaiChong/FlyingCaiChong.github.io.git
-git add -A 
+git remote add origin git@github.com:FlyingCaiChong/FlyingCaiChong.github.io.git
+git config user.email "951412526@qq.com"
+git add -A
 git commit -m "deploy"
-
+echo "准备push代码"
 git push -f origin master
+# Enter passphrase for key '/Users/fangheng/.ssh/id_ed25519':
+# 密码: 123456
